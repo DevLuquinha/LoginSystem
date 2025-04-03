38a155b2e1f27d03d5371f212195ad09e02ad39e
Outro Lucas <lucasemmanuelrodrigues737@gmail.com>
2025-04-03 11:06:53 -0300

    New example class, A false copy the important class SecureConfigurations because this class contains importants info and this class is in the git ignore

---
+++ LoginSystem/SecureConfigurationsExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem
+{
+    // Classe de exemplo da classe original, porém com um valor diferente!
+    internal class SecureConfigurationsExample
+    {
+        // Chave api para autenticação com o banco de dados firestore
+        public static string ApiKeyFirestore { get; } = "Bla bla bla";
+    }
+}
